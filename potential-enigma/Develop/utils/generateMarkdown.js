02d9fe7c9c7ddaa98380928a3d38e95dae96fee8
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-07-26 23:08:25 -0500

    Update generateMarkdown.js

--- potential-enigma/Develop/utils/generateMarkdown.js
+++ potential-enigma/Develop/utils/generateMarkdown.js
@@ -1,19 +1,47 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Import required packages
+const { renderLicenseBadge, renderLicenseLink, renderLicenseSection } = require('./licenses');
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  // Generate license badge and link
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+
   return `# ${data.title}
 
+${licenseBadge}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+${renderLicenseSection(data.license)}
+${licenseLink}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For any questions, please contact me:
+GitHub: [${data.github}](https://github.com/${data.github})
+Email: ${data.email}
 `;
 }
 
